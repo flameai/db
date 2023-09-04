2b23d00d7284cbdb8d454b7449bef73d4f5fdfa3
Alexander <aandryukov@more.tv>
2023-09-04 10:15:04 +0500

    redis registry implemented

--- common/db/redis/fastapi.py
+++ common/db/redis/fastapi.py
@@ -1,10 +1,11 @@
 from common.fastapi.base import AppEventProvidedComponent, ComponentProvidedApp
 from common.db.redis.async_redis import redis
+from common.db.redis.fabric import RedisMethodsFabric
 
 
 class Redis(AppEventProvidedComponent):
     async def startup(self, app: ComponentProvidedApp) -> None:
-        app.config["hash_db"] = redis
+        app.config["hash_db"] = RedisMethodsFabric(redis)
 
     async def shutdown(self, app: ComponentProvidedApp) -> None:
         await app.redis.close()
